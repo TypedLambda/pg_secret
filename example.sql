a52419d38d189a0d72c8ad1a86fbdc0c45a9e55d
Dan Draper <dan@coderdan.co>
2020-01-27 17:35:01 +1100

    Updated example

--- example.sql
+++ example.sql
@@ -1,7 +1,7 @@
-CREATE TABLE secret_test (name text, phone secret, email secret);
+CREATE TABLE secret_test (name text, income secret, email secret);
 
 CREATE INDEX secret_phone_ind ON secret_test
-   USING btree(phone secret_btree_ops);
+   USING btree(income secret_btree_ops);
 
 CREATE INDEX secret_email_ind ON secret_test
    USING btree(email secret_btree_ops);
